840ef97626879f5898fd8b247dafbbca636df270
DR-MARTIN24 <140517300+DR-MARTIN24@users.noreply.github.com>
2026-01-14 05:53:11 +0800

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,8 @@
+// ===== Pro Player v3.0 配置 =====
+
+const SUPABASE_URL = "https://ztesoqtztyheezualipa.supabase.co;
+const SUPABASE_ANON_KEY = "sb_publishable_xCKbicWQaYM1knmkOqH-QA_3l5Xr96T";
+
+const BRAND_NAME = "Pro Player";
+const VERSION = "v3.0";
+const VERSION_TIME = "2026-01-14 23:18:00";
